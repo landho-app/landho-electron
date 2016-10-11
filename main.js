d52f75848e4bcf5ab72b5404ab028d20991834ec
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-10-11 13:54:49 +0200

    start with auto updater

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-var app = require("app"); // Module to control application life.
-var BrowserWindow = require("browser-window"); // Module to create native browser window.
+var app = require("app");
+var os = require("os");
+var browserWindow = require("browser-window");
+var autoUpdater = require("auto-updater");
 
-// Keep a global reference of the window object, if you don"t, the window will
+var platform = os.platform() + "_" + os.arch();
+var version = app.getVersion();
+
+autoUpdater.setFeedURL("http://update.landho-app.com/update/" + platform + "/" + version);
+
+// keep a global reference of the window object, if you don"t, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function() {
-	// On OS X it is common for applications and their menu bar
+
+	// on OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform != "darwin") {
 		app.quit();
@@ -17,8 +25,9 @@ app.on("window-all-closed", function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on("ready", function() {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({
+
+	// create the browser window.
+	mainWindow = new browserWindow({
 		"width": 1200,
 		"height": 800,
 		"node-integration": false
@@ -28,9 +37,10 @@ app.on("ready", function() {
 	mainWindow.loadURL("file://" + __dirname + "/landho-core/index.html");
 	//mainWindow.toggleDevTools();
 
-	// Emitted when the window is closed.
+	// emitted when the window is closed.
 	mainWindow.on("closed", function() {
-		// Dereference the window object, usually you would store windows
+
+		// dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
 		mainWindow = null;
