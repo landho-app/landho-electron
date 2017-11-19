f16c97bf3c8f53e3b2203d74e9cfec24997fcac7
Thomas Brüggemann <mail@thomasbrueggemann.com>
2017-11-19 23:44:05 +0100

    no message

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function() {
-
 	// on OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform != "darwin") {
@@ -18,20 +17,23 @@ app.on("window-all-closed", function() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on("ready", function() {
-
 	// create the browser window.
 	mainWindow = new BrowserWindow({
-		"width": 1200,
-		"height": 800
+		width: 1200,
+		height: 800
 	});
 
 	// and load the index.html of the app.
-	mainWindow.loadURL("file://" + __dirname + "/landho-core/index.html?version=" + process.env.npm_package_version);
+	mainWindow.loadURL(
+		"file://" +
+			__dirname +
+			"/landho-core/index.html?version=" +
+			process.env.npm_package_version
+	);
 	//mainWindow.toggleDevTools();
 
 	// emitted when the window is closed.
 	mainWindow.on("closed", function() {
-
 		// dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
